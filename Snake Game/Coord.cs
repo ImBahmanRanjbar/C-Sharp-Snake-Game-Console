01c77040d3884f574d2bca2fefc426eeba8d2d0b
Bahman <imbahmanranjbar@gmail.com>
2024-08-30 00:06:33 +0330

    Add files via upload

---
+++ Snake Game/Coord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    class Coord
+    {
+        private int x, y;
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+        public Coord(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj==null)||  !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Coord other =(Coord)obj;
+            return x == other.x && y == other.y;
+        }
+        public void ApplyMovementDirection(Direction direction)
+        {
+            switch (direction)
+            {
+                    case Direction.Up:
+                    y--;
+                        break;
+                    case Direction.Down:
+                    y++;
+                        break;
+                    case Direction.Left:
+                    x--;
+                        break;
+                    case Direction.Right:
+                    x++;
+                        break;
+                    
+            }
+        }
+    }
+}
---
+++ Snake Game/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Snake_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Beep();
+            Console.Beep();
+            Console.Title = "Snake Game by ImBahmanRanjbar";
+
+
+            Coord gridDimensions = new Coord(50, 20);
+            Coord snakePos = new Coord(10, 1);
+            Random r = new Random();
+            Coord applePos = new Coord(r.Next(1, gridDimensions.X - 1), r.Next(1, gridDimensions.Y - 1));
+            int framedelaymilli = 100;
+            Direction movementDirection = Direction.Down;
+            List<Coord> snakePosHistory = new List<Coord>();
+            int tailLenght = 0;
+            int Score = 0;
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Score :  "+Score);
+                snakePos.ApplyMovementDirection(movementDirection);
+                for (int i = 0; i < gridDimensions.Y; i++)
+                {
+
+                    for (int j = 0; j < gridDimensions.X; j++)
+                    {
+                        Coord currentCoord = new Coord(j, i);
+                        if (snakePos.Equals(currentCoord))
+                        {
+                            Console.Write("■");
+                        }
+                        else if (applePos.Equals(currentCoord) || snakePosHistory.Contains(currentCoord))
+                        {
+                            Console.Write("■");
+                        }
+                        else if (j == 0 || i == 0 || j == gridDimensions.X - 1 || i == gridDimensions.Y - 1)
+                        {
+                            Console.Write("#");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                if (snakePos.Equals(applePos))
+                {
+                   
+                    tailLenght++;
+                    Score++;
+                    applePos = new Coord(r.Next(1, gridDimensions.X - 1), r.Next(1, gridDimensions.Y - 1));
+                }
+                else if (snakePos.X==0||snakePos.Y==0||snakePos.X==gridDimensions.X-1||snakePos.Y==gridDimensions.Y-1||snakePosHistory.Contains(snakePos))
+                { Console.Beep();
+                    Score = 0;
+                    tailLenght = 0;
+                    snakePos = new Coord(10, 1);
+                    snakePosHistory.Clear();
+                    movementDirection = Direction.Down;
+                    continue;
+                }
+                snakePosHistory.Add(new Coord(snakePos.X, snakePos.Y));
+                if (snakePosHistory.Count>tailLenght)
+                {
+                    snakePosHistory.RemoveAt(0);
+                }
+
+                DateTime time = DateTime.Now;
+                while ((DateTime.Now-time).Milliseconds<framedelaymilli)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        ConsoleKey key = Console.ReadKey().Key;
+                        switch (key)
+                        {
+                            case ConsoleKey.LeftArrow:
+                                movementDirection = Direction.Left;
+                                break;
+                            case ConsoleKey.RightArrow:
+                                movementDirection = Direction.Right;
+                                break;
+                            case ConsoleKey.UpArrow:
+                                movementDirection = Direction.Up;
+                                break;
+                            case ConsoleKey.DownArrow:
+                                movementDirection = Direction.Down;
+                                break;
+                        }
+                    }
+                }
+                  
+            }
+        }
+    }
+}
